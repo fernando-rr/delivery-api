3eccb84e62136854338bcb6e6f7f107aca52fafb
fernando-rr <fernando.r.rinaldi@gmail.com>
2025-11-17 08:11:41 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Delivery WEB',
+        'message' => 'Delivery WEB TEST',
         'version' => '1.0.0',
     ]);
 });
