51a5cfbe906b8ce2cfb12832690cbf43d9eb94f8
fernando-rr <fernando.r.rinaldi@gmail.com>
2025-11-17 22:49:27 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 // Redireciona todas as rotas web para 404
 // O front-end será servido aqui no futuro
 Route::fallback(function () {
-    abort(404, 'Rota não encontrada. Use as rotas /api para acessar a API.');
+    abort(404, 'Rota não encontrada.');
 });
